051435424a16af722185dfb486bfc57b8d5cc362
mariannemagnussen <marianne_magnussen@icloud.com>
2019-02-22 10:00:23 +0100

    Revert "Login"

--- src/index.js
+++ src/index.js
@@ -2,17 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Student from './components/Student/Student';
-import TeacherWithAuth from './components/Teacher/TeacherWithAuth';
+import Teacher from './components/Teacher/Teacher';
 import App from './components/App/App';
 import './index.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
 
 ReactDOM.render(
     <Router>
         <div>
             <Route exact path="/" component={App} />  {/* Remove exact if Title component should be displayed for all routes */}
-            <Route exact path="/teacher" component={TeacherWithAuth} />
+            <Route exact path="/teacher" component={Teacher} />
             <Route exact path="/student" component={Student} />
         </div>
     </Router>,
