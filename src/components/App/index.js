ef0b10612f17404ac7bcb32e4681285b410780d6
Marianne Magnussen <mariannemagnussen@Mariannes-MacBook-Pro.local>
2019-02-24 10:29:18 +0100

    Change colors

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -13,7 +13,7 @@ const App = () => {
       
       <Row className="App-buttons">
         <Col xs lg="3">
-         <Link to="/student" style={{ textDecoration: 'none' }}><Button className="btn-classPin" variant="outline-light" size="lg" block>Jeg er en elev!</Button></Link>
+         <Link to="/student" style={{ textDecoration: 'none' }}><Button className="btn-class" variant="outline-light" size="lg" block>Jeg er en elev!</Button></Link>
         </Col>
         <Col xs lg="3">
         <Link to="/teacher" style={{ textDecoration: 'none' }}><Button className="btn-submit" variant="outline-light" size="lg" block>Jeg er en lærer!</Button></Link>
