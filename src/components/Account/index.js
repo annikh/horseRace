5997d52c00d49d41717e913fe5bb23af437d5dbe
Anniken Holst <annikekh@stud.ntnu.no>
2019-02-25 17:24:36 +0100

    Handle auth state with React Context API, and wrap teacher component in withAuthentication so that teacher does not handle authentication

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import Navigation from '../Navigation';
 import './style.css';
 
-
 const Account = () => (
   <div>
-    <Navigation/>
     <div className="accountBody">
       <h1>Account</h1>
     </div>
