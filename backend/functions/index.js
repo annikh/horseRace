0806563a616bef0cbd0b2b1a653af6b45b62a389
Anniken Holst <annikekh@stud.ntnu.no>
2019-02-27 16:38:03 +0100

    Enable cors

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true });
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
