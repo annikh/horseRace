d5ef13933cca2f65fbbb9dec606023d07b7b2b7f
Anniken Holst <annikekh@stud.ntnu.no>
2019-02-27 18:51:22 +0100

    Create post and get functions for classrooms

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,9 +1,66 @@
 const functions = require('firebase-functions');
 const cors = require('cors')({ origin: true });
+const admin = require('firebase-admin');
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+admin.initializeApp();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+const database = admin.database().ref('/classrooms');
+
+exports.addClassroom = functions.https.onRequest((req, res) => {
+  return cors(req, res, () => {
+    if(req.method !== 'POST') {
+      return res.status(401).json({
+        message: 'Not allowed'
+      })
+    }
+    console.log(req.body)
+
+    database.push(req.body);
+
+    let classrooms = [];
+
+    return database.on('value', (snapshot) => {
+      snapshot.forEach((classroom) => {
+        classrooms.push({
+          id: classroom.key,
+          pin: classroom.val().pin,
+          names: classroom.val().names
+        });
+      });
+      
+      res.status(200).json(classrooms)
+    }, (error) => {
+      res.status(error.code).json({
+        message: `Something went wrong. ${error.message}`
+      })
+    })
+  })
+})
+
+exports.getClassrooms = functions.https.onRequest((req, res) => {
+    return cors(req, res, () => {
+        if(req.method !== 'GET') {
+          return res.status(404).json({
+            message: 'Not allowed'
+          })
+        }
+
+        let classrooms = [];
+
+        return database.on('value', (snapshot) => {
+            snapshot.forEach((classroom) => {
+              classrooms.push({
+                id: classroom.key,
+                pin: classroom.val().pin,
+                names: classroom.val().names
+              });
+            });
+
+        res.status(200).json(classrooms)
+        }, (error) => {
+            res.status(error.code).json({
+            message: `Something went wrong. ${error.message}`
+            })
+        })
+    })
+})
