08da722697a1228f4c789adee26d7230e2e49f50
Anniken Holst <annikekh@stud.ntnu.no>
2019-02-27 11:52:56 +0100

    Protect navigation and display username in nav bar

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,32 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import SignOutButton from '../SignOut';
 import { Nav } from 'react-bootstrap';
-import { AuthUserContext } from '../Session';
+import { AuthUserContext, withAuthorization } from '../Session';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './style.css';
 
 const Navigation = ({match}) => (
-  <div>
-    <AuthUserContext.Consumer>
-      {authUser =>
-        authUser ? <NavigationAuth match={match}/> : null
-      }
-    </AuthUserContext.Consumer>
-  </div>
+  <AuthUserContext.Consumer>
+    {authUser => (
+      <div>
+        <Nav className="navbar">
+          <Nav.Item>
+            <Link to={match.url} className="link"><FontAwesomeIcon icon="user" color="black"/> {authUser.email}</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link to={`${match.url}/create-classroom`} className="link">Klasserom</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <SignOutButton />
+          </Nav.Item>
+        </Nav>
+      </div>
+    )}
+  </AuthUserContext.Consumer>
 );
 
-const NavigationAuth = ({match}) => (
-  <Nav className="navbar">
-    <Nav.Item>
-      <Link to={match.url} className="link"><FontAwesomeIcon icon="user" color="black"/>*brukernavn*</Link>
-    </Nav.Item>
-    <Nav.Item>
-      <Link to={`${match.url}/create-classroom`} className="link">Klasserom</Link>
-    </Nav.Item>
-    <Nav.Item>
-      <SignOutButton />
-    </Nav.Item>
-  </Nav>
-);
+const condition = authUser => !!authUser;
 
-export default Navigation;
+export default withAuthorization(condition)(Navigation);
