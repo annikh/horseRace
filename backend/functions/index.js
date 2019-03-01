2591ec3a726ae3f5fbb2126a8bc6c94327a2d8cc
Marianne Magnussen <mariannemagnussen@Mariannes-MacBook-Pro.local>
2019-03-01 08:57:23 +0100

    Add functions for adding and getting games

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -4,7 +4,9 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-const database = admin.database().ref('/classrooms');
+const classroomDB = admin.database().ref('/classrooms');
+const gameDB = admin.database().ref('/games');
+
 
 exports.addClassroom = functions.https.onRequest((req, res) => {
   return cors(req, res, () => {
@@ -14,38 +16,85 @@ exports.addClassroom = functions.https.onRequest((req, res) => {
       })
     }
     console.log(req.body)
-    database.push(req.body);
+    classroomDB.push(req.body);
     getClassroomsFromDatabase(res);
   })
 })
 
 exports.getClassrooms = functions.https.onRequest((req, res) => {
-    return cors(req, res, () => {
-        if(req.method !== 'GET') {
-          return res.status(404).json({
-            message: 'Not allowed'
-          })
-        }
-        getClassroomsFromDatabase(res);
-    })
+  return cors(req, res, () => {
+      if(req.method !== 'GET') {
+        return res.status(404).json({
+          message: 'Not allowed'
+        })
+      }
+      getClassroomsFromDatabase(res);
+  })
 })
 
 const getClassroomsFromDatabase = (res) => {
-    let classrooms = [];
+  let classrooms = [];
 
-    return database.on('value', (snapshot) => {
-        snapshot.forEach((classroom) => {
-            classrooms.push({
-            id: classroom.key,
-            pin: classroom.val().pin,
-            names: classroom.val().names
-            });
-        });
+  return classroomDB.on('value', (snapshot) => {
+      snapshot.forEach((classroom) => {
+          classrooms.push({
+          id: classroom.key,
+          pin: classroom.val().pin,
+          names: classroom.val().names
+          });
+      });
 
-    res.status(200).json(classrooms)
-    }, (error) => {
-        res.status(error.code).json({
-        message: `Something went wrong. ${error.message}`
+  res.status(200).json(classrooms)
+  }, (error) => {
+      res.status(error.code).json({
+      message: `Something went wrong. ${error.message}`
+      })
+  })
+};
+
+
+exports.addGame = functions.https.onRequest((req, res) => {
+  return cors(req, res, () => {
+    if(req.method !== 'POST') {
+      return res.status(401).json({
+        message: 'Not allowed'
+      })
+    }
+    console.log("Add Game Backend: ", req.body)
+    gameDB.push(req.body);
+    getGamesFromDatabase(res);
+  })
+})
+
+
+exports.getGames = functions.https.onRequest((req, res) => {
+  return cors(req, res, () => {
+      if(req.method !== 'GET') {
+        return res.status(404).json({
+          message: 'Not allowed'
         })
-    })
-  };
+      }
+      getGamesFromDatabase(res);
+  })
+})
+
+const getGamesFromDatabase = (res) => {
+  let games = [];
+
+  return gameDB.on('value', (snapshot) => {
+      snapshot.forEach((game) => {
+          games.push({
+          id: game.key,
+          classroom_id: game.val().classroom_id,
+          user_id: game.val().user_id,
+          date: new Date()
+          });
+      });
+
+  res.status(200).json(classrooms)
+  }, (error) => {
+      res.status(error.code).json({
+      message: `Something went wrong. ${error.message}`
+      })
+  })
+};
