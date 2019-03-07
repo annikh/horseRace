8633d8142933824b8515664c97562220257dc0fe
Anniken Holst <annikekh@stud.ntnu.no>
2019-03-07 12:59:41 +0100

    Remove faulty this

--- web/src/objects/Game.js
+++ web/src/objects/Game.js
@@ -11,7 +11,7 @@ class Game {
     constructor(pin, user_id, classroom_id, score_board) {
         this.id = '';
         this.date = '';
-        this.score_board = this.score_board;
+        this.score_board = score_board;
         this.pin = pin;
         this.user_id = user_id;
         this.classroom_id = classroom_id;
