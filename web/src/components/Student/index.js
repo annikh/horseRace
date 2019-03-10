773b25e1b54bec1b29b87500aa818deb96c92e71
Marianne Magnussen <marianne_magnussen@icloud.com>
2019-03-10 11:32:36 +0100

    remove log warnings

--- web/src/components/Student/index.js
+++ web/src/components/Student/index.js
@@ -1,45 +1,45 @@
-import React, { Component } from 'react';
-import { Button, Form, Row, Col } from 'react-bootstrap';
-import './style.css';
+import React, { Component } from "react";
+import { Button, Form, Row, Col } from "react-bootstrap";
+import "./style.css";
 
 class Student extends Component {
-
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      value: '',
+      value: "",
       pinEntered: false,
-      buttonValue: 'Enter',
-      placeholder: 'Skriv inn PIN'
-    }
+      buttonValue: "Enter",
+      placeholder: "Skriv inn PIN"
+    };
     this.handleEnterClassroomPin = this.handleEnterClassroomPin.bind(this);
     this.handleEnterStudentName = this.handleEnterStudentName.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
-  
+
   handleEnterClassroomPin() {
-    const pin = this.state.value;
     this.setState({
       pinEntered: true,
-      buttonValue: 'Bli med!',
-      placeholder:'Skriv inn navnet ditt',
-      value: ''
-    })
+      buttonValue: "Bli med!",
+      placeholder: "Skriv inn navnet ditt",
+      value: ""
+    });
   }
 
   handleEnterStudentName() {
     const name = this.state.value;
-    alert('Gratulerer ' + name + ', du er inne!');
+    alert("Gratulerer " + name + ", du er inne!");
     //Redirect to classroom
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({ value: event.target.value });
   }
 
   handleSubmit(event) {
-    this.state.pinEntered ? this.handleEnterStudentName() : this.handleEnterClassroomPin();
+    this.state.pinEntered
+      ? this.handleEnterStudentName()
+      : this.handleEnterClassroomPin();
     event.preventDefault();
   }
 
@@ -48,25 +48,38 @@ class Student extends Component {
   };
 
   render() {
-    const isInvalid = this.state.value === '';
+    const isInvalid = this.state.value === "";
 
     return (
-        <Form className="Student" onSubmit={this.handleSubmit}>
-          <Row>
-            <Col>
-              <Form.Label><h2>Bli med klassen din og spill!</h2></Form.Label>
-            </Col>
-          </Row>
-          <Row>
-            <Col md="auto">
-              <Form.Control placeholder={this.state.placeholder} value={this.state.value} onChange={this.handleChange}/>
-            </Col>
-            <Col>
-              <Button className="btn-classPin" disabled={isInvalid} variant="outline-light" type="submit">{this.state.buttonValue}</Button>
-            </Col>
-          </Row>
-        </Form>
-    )
+      <Form className="Student" onSubmit={this.handleSubmit}>
+        <Row>
+          <Col>
+            <Form.Label>
+              <h2>Bli med klassen din og spill!</h2>
+            </Form.Label>
+          </Col>
+        </Row>
+        <Row>
+          <Col md="auto">
+            <Form.Control
+              placeholder={this.state.placeholder}
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </Col>
+          <Col>
+            <Button
+              className="btn-classPin"
+              disabled={isInvalid}
+              variant="outline-light"
+              type="submit"
+            >
+              {this.state.buttonValue}
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    );
   }
 }
 
--- web/src/index.js
+++ web/src/index.js
@@ -1,36 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Student from './components/Student';
-import Teacher from './components/Teacher'; 
-import App from './components/App';
-import SignUpPage from './components/SignUp';
-import SignInPage from './components/SignIn';
-import PasswordForgetPage from './components/PasswordForget';
-import Firebase, {FirebaseContext} from './components/Firebase';
-import './index.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import * as ROUTES from './constants/routes';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Student from "./components/Student";
+import Teacher from "./components/Teacher";
+import App from "./components/App";
+import SignUpPage from "./components/SignUp";
+import SignInPage from "./components/SignIn";
+import PasswordForgetPage from "./components/PasswordForget";
+import Firebase, { FirebaseContext } from "./components/Firebase";
+import "./index.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import * as ROUTES from "./constants/routes";
 
 /* Add Font Awesome Icons */
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 library.add(faUser);
 
 ReactDOM.render(
-    <FirebaseContext.Provider value={new Firebase()}>
-        <Router>
-        <div>
-            <Route exact path={ROUTES.LANDING} component={App} /> 
-            <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-            <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-            <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-            <Route path={ROUTES.STUDENT} component={Student} />
-            <Route path={ROUTES.TEACHER} component={Teacher} />
-        </div>
-        </Router>
-    </FirebaseContext.Provider>
-    ,
-    document.getElementById('root')
+  <FirebaseContext.Provider value={new Firebase()}>
+    <Router>
+      <div>
+        <Route exact path={ROUTES.LANDING} component={App} />
+        <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+        <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+        <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+        <Route path={ROUTES.STUDENT} component={Student} />
+        <Route path={ROUTES.TEACHER} component={Teacher} />
+      </div>
+    </Router>
+  </FirebaseContext.Provider>,
+  document.getElementById("root")
 );
