15d82f1ab9d713497913bc1ab54bcd0fc037ea41
Anniken Holst <annikekh@stud.ntnu.no>
2019-03-27 12:32:00 +0100

    Display random modal header

--- web/src/components/Editor/index.js
+++ web/src/components/Editor/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import AceEditor from 'react-ace';
-import { Button, Container, Form } from 'react-bootstrap';
+import { Button, Modal, Form } from 'react-bootstrap';
 import axios from 'axios';
 
 import 'brace/mode/python';
@@ -11,16 +11,22 @@ class Editor extends Component {
     constructor(props, context) {
         super(props, context);
         
-        this.onChange = this.onChange.bind(this);
+        this.handleChange = this.handleChange.bind(this);
         this.runCode = this.runCode.bind(this);
+        this.handleModalClose = this.handleModalClose.bind(this);
+        this.handleModalShow = this.handleModalShow.bind(this);
 
         this.state = {
             aceEditorValue: '# Enter your code here.',
-            output: ''
+            output: '',
+            error_message: '',
+            showModal: false,
+            modalHeaders: ['Prøv igjen!', 'Bedre lykke neste gang!', 'Dette gikk visst ikke helt etter planen.', 'Oops..', 'Ikke helt der ennå..'],
+            modalHeaderText: ''
         }
     }
 
-    onChange(value) {
+    handleChange(value) {
         this.setState({ aceEditorValue: value });
     }
 
@@ -31,24 +37,50 @@ class Editor extends Component {
         axios.get('http://127.0.0.1:5000/hei', { params: { code: this.state.aceEditorValue } })
         .then( response => {
           console.log(response)
-          this.setState({output: response.data.output})
-          let error_message = response.data.error_message
-          if (error_message) alert(error_message);
+          this.setState({output: response.data.output, error_message: response.data.error_message})
+          if (this.state.error_message) this.handleModalShow()
         })
         .catch(function(error) {
           console.log(error);
         });
     } 
 
+    handleModalClose() {
+        this.setState({showModal: false})
+    }
+
+    handleModalShow() {
+        this.setState({showModal: true, modalHeaderText: this.state.modalHeaders[Math.floor(Math.random()*this.state.modalHeaders.length)]})
+    }
+
+    ErrorModal = () => (
+        <Modal show={this.state.showModal} onHide={this.handleModalClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>
+                {this.state.modalHeaderText}
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            {this.state.error_message}
+          </Modal.Body>
+          {/* <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleModalClose}>
+              Close
+            </Button>
+          </Modal.Footer> */}
+        </Modal>
+    )
+
     render() {
         return(
             <div>
+            <this.ErrorModal/>
             <AceEditor
                 placeholder="Placeholder Text"
                 mode="python"
                 theme="monokai"
                 name="UNIQUE_ID_OF_DIV"
-                onChange={this.onChange}
+                onChange={this.hanldeChange}
                 fontSize={14}
                 showPrintMargin={true}
                 showGutter={true}
