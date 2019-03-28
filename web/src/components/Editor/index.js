97028d7e17de3e2417398c06ca8788c8f3cefb49
Anniken Holst <annikekh@stud.ntnu.no>
2019-03-28 11:10:19 +0100

    Refactor out run code and console from editor to game. Now both Cards and Editor are separate child components of Game

--- web/src/components/Editor/index.js
+++ web/src/components/Editor/index.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import AceEditor from 'react-ace';
-import { Button, Modal, Form } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import axios from 'axios';
 
 import 'brace/mode/python';
 import 'brace/theme/monokai';
@@ -13,21 +12,10 @@ class Editor extends Component {
         super(props, context);
         
         this.handleChange = this.handleChange.bind(this);
-        this.runCode = this.runCode.bind(this);
-        this.handleErrorModalClose = this.handleErrorModalClose.bind(this);
-        this.handleErrorModalShow = this.handleErrorModalShow.bind(this);
-        this.handleSolvedModalClose = this.handleSolvedModalClose.bind(this);
-        this.handleSolvedModalShow = this.handleSolvedModalShow.bind(this);
+        this.handleButtonClick = this.handleButtonClick.bind(this);
 
         this.state = {
-            aceEditorValue: '# Enter your code here.',
-            output: '',
-            error_message: '',
-            showErrorModal: false,
-            showSolvedModal: false,
-            errorModalHeaders: ['Prøv igjen!', 'Bedre lykke neste gang!', 'Dette gikk visst ikke helt etter planen.', 'Oops..', 'Ikke helt der ennå..'],
-            errorModalHeaderText: '',
-            currentTask: {difficulty: 1, test: "assert hei('Anniken') == 'Anniken', 'Returnerer funksjonen din navnet som skrives inn?'", text: "Lag en funksjon med navn 'hei' som tar inn et navn og returnerer det.", title: 'hei', error_hint: 'Har du husket å definere en funksjon som heter "hei"?'}
+            aceEditorValue: '# Enter your code here.'
         }
     }
 
@@ -35,63 +23,10 @@ class Editor extends Component {
         this.setState({ aceEditorValue: value });
     }
 
-    runCode(event) {
+    handleButtonClick(event) {
         event.preventDefault();
-        console.log(this.state.aceEditorValue);
-        // axios.get('http://python-eval-server.appspot.com/run', { params: { code: this.state.aceEditorValue } })
-        axios.get('http://127.0.0.1:5000/run', { params: { code: this.state.aceEditorValue, task: this.state.currentTask } })
-        .then( response => {
-          console.log(response)
-          this.setState({output: response.data.output, error_message: response.data.error_message})
-          if (this.state.error_message !== '') this.handleErrorModalShow()
-          if (response.data.solved) this.handleSolvedModalShow()
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
+        this.props.onRunCode(this.state.aceEditorValue)
     } 
-
-    handleErrorModalClose() {
-        this.setState({showErrorModal: false})
-    }
-
-    handleErrorModalShow() {
-        this.setState({showErrorModal: true, errorModalHeaderText: this.state.errorModalHeaders[Math.floor(Math.random()*this.state.errorModalHeaders.length)]})
-    }
-
-    handleSolvedModalClose() {
-        this.setState({showSolvedModal: false})
-    }
-
-    handleSolvedModalShow() {
-        this.setState({showSolvedModal: true})
-    }
-
-    SolvedModal = () => (
-        <Modal show={this.state.showSolvedModal} onHide={this.handleSolvedModalClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>
-                Bra jobbet!
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            Du fikk til oppgaven :D
-          </Modal.Body>
-        </Modal>
-    )
-
-    ErrorModal = () => (
-        <Modal show={this.state.showErrorModal} onHide={this.handleErrorModalClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>
-                {this.state.errorModalHeaderText}
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            {this.state.error_message}
-          </Modal.Body>
-        </Modal>
-    )
 
     render() {
         return(
@@ -111,11 +46,7 @@ class Editor extends Component {
                     showLineNumbers: true,
                     tabSize: 2,
                     }}/>
-                <Button variant="success" onClick={this.runCode} ><FontAwesomeIcon icon="play"/> Run</Button>
-                <this.ErrorModal/>
-                <this.SolvedModal/>
-                <br/><br/>
-                <Form.Control as="textarea" readOnly style={{backgroundColor: '#262722', color: '#aaaaaa', height: "20vh", width: "80vh"}} value={"Output: " + this.state.output}/>
+                <Button variant="success" onClick={this.handleButtonClick} ><FontAwesomeIcon icon="play"/> Run</Button>
             </div>
         )
     }
--- web/src/components/Game/index.js
+++ web/src/components/Game/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
+import axios from 'axios';
 import { withFirebase } from "../Firebase";
 import {
   Container,
   Row,
   Col,
+  Modal,
+  Form
 } from "react-bootstrap";
 import Editor from "../Editor";
 import Cards from "../Cards";
@@ -13,15 +16,43 @@ class Game extends Component {
   constructor(props) {
     super(props);
 
+    this.runCode = this.runCode.bind(this);
+    this.handleErrorModalClose = this.handleErrorModalClose.bind(this);
+    this.handleErrorModalShow = this.handleErrorModalShow.bind(this);
+    this.handleSolvedModalClose = this.handleSolvedModalClose.bind(this);
+    this.handleSolvedModalShow = this.handleSolvedModalShow.bind(this);
+
     this.state = {
       cards: [],
       showCard: false,
       selectedCard: {
         title: "",
         text: ""
-      }
+      },
+      output: '',
+      error_message: '',
+      showErrorModal: false,
+      showSolvedModal: false,
+      errorModalHeaders: ['Prøv igjen!', 'Bedre lykke neste gang!', 'Dette gikk visst ikke helt etter planen.', 'Oops..', 'Ikke helt der ennå..'],
+      errorModalHeaderText: '',
+      currentTask: {difficulty: 1, test: "assert hei('Anniken') == 'Anniken', 'Returnerer funksjonen din navnet som skrives inn?'", text: "Lag en funksjon med navn 'hei' som tar inn et navn og returnerer det.", title: 'hei', error_hint: 'Har du husket å definere en funksjon som heter "hei"?'}
     };
   }
+
+  runCode(submittedCode) {
+    console.log(this.state.aceEditorValue);
+    // axios.get('http://python-eval-server.appspot.com/run', { params: { code: this.state.aceEditorValue } })
+    axios.get('http://127.0.0.1:5000/run', { params: { code: submittedCode, task: this.state.currentTask } })
+    .then( response => {
+      console.log(response)
+      this.setState({output: response.data.output, error_message: response.data.error_message})
+      if (this.state.error_message !== '') this.handleErrorModalShow()
+      if (response.data.solved) this.handleSolvedModalShow()
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+  } 
 
   solveTask(solutionCode) {
     //Sjekk mot redux store currentGame -> Tasks -> lik ID -> solutionCode
@@ -29,13 +60,59 @@ class Game extends Component {
     //               disable oppgaven for spillet = bytt ut oppgaven med del av bilde
   }
 
+  handleErrorModalClose() {
+      this.setState({showErrorModal: false})
+  }
+
+  handleErrorModalShow() {
+      this.setState({showErrorModal: true, errorModalHeaderText: this.state.errorModalHeaders[Math.floor(Math.random()*this.state.errorModalHeaders.length)]})
+  }
+
+  handleSolvedModalClose() {
+      this.setState({showSolvedModal: false})
+  }
+
+  handleSolvedModalShow() {
+      this.setState({showSolvedModal: true})
+  }
+
+  SolvedModal = () => (
+      <Modal show={this.state.showSolvedModal} onHide={this.handleSolvedModalClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+              Bra jobbet!
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Du fikk til oppgaven :D
+        </Modal.Body>
+      </Modal>
+  )
+
+  ErrorModal = () => (
+      <Modal show={this.state.showErrorModal} onHide={this.handleErrorModalClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+              {this.state.errorModalHeaderText}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {this.state.error_message}
+        </Modal.Body>
+      </Modal>
+  )
+
   render() {
     console.log(this.props.game);
     return (
       <Container className="gameComponent">
+      <this.ErrorModal/>
+      <this.SolvedModal/>
       <Row>
         <Col style={{justifyContent: "left"}}>
-          <Editor />
+          <Editor onRunCode={this.runCode}/>
+          <br/><br/>
+          <Form.Control as="textarea" readOnly style={{backgroundColor: '#262722', color: '#aaaaaa', height: "20vh", width: "80vh"}} value={"Output: " + this.state.output}/>
         </Col>
         <Col>
           <Cards cookies={this.props.cookies} />
