cb7152cfafd44d5500a8fb584209fd490761a7d5
Marianne Magnussen <marianne_magnussen@icloud.com>
2019-04-11 09:10:24 +0200

    show modal with team players

--- web/src/components/TeacherClassrooms/index.js
+++ web/src/components/TeacherClassrooms/index.js
@@ -1,16 +1,23 @@
 import React, { Component } from "react";
 import { AuthUserContext, withAuthorization } from "../Session";
-import { Container, Row, Col, ListGroup } from "react-bootstrap";
+import { Container, Row, Col, ListGroup, Modal, Button } from "react-bootstrap";
 import { withFirebase } from "../Firebase";
 import CreateClassroom from "../CreateClassroom";
 
 class TeacherClassrooms extends Component {
   constructor(props) {
     super(props);
+    this.showClassroom = this.showClassroom.bind(this);
+    this.classroomList = this.classroomList.bind(this);
+    this.deleteClassroom = this.deleteClassroom.bind(this);
+    this.classroom = this.classroom.bind(this);
+    this.nameList = this.nameList.bind(this);
 
     this.state = {
       classroomName: "",
-      classrooms: []
+      classrooms: [],
+      showClassroom: false,
+      selectedClassroom: ""
     };
   }
 
@@ -21,6 +28,80 @@ class TeacherClassrooms extends Component {
       this.setState({ loading: false, classrooms: snapshot.val() });
     });
   }
+
+  showClassroom(event) {
+    console.log(event);
+    event.preventDefault();
+    const classroom = event.target.value;
+    const show = !this.state.showClassroom;
+    this.setState({
+      showClassroom: show,
+      selectedClassroom: classroom
+    });
+  }
+
+  deleteClassroom() {}
+
+  classroom = classroom => {
+    console.log(classroom);
+    return (
+      <Modal onHide={this.showClassroom}>
+        <Modal.Header closeButton>
+          <Modal.Title>{this.state.selectedClassroom}}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{this.nameList(classroom.names)}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={this.handleClose}>
+            Close
+          </Button>
+          <Button variant="danger" onClick={this.deleteClassroom}>
+            Slett klasserom
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  };
+
+  nameList = names => {
+    console.log(names);
+    return (
+      <ListGroup variant="flush" style={{ width: "80%" }}>
+        {names.map((name, i) => (
+          <ListGroup.Item
+            key={i}
+            style={{ textAlign: "left" }}
+            action
+            variant="warning"
+          >
+            <Row>
+              <Col>{name}</Col>
+            </Row>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+    );
+  };
+
+  classroomList = classrooms => {
+    return (
+      <ListGroup variant="flush" style={{ width: "80%" }}>
+        {Object.keys(classrooms).map((name, i) => (
+          <ListGroup.Item
+            key={i}
+            style={{ textAlign: "left" }}
+            action
+            variant="warning"
+            onClick={this.showClassroom}
+            value={name}
+          >
+            <Row>
+              <Col>{name}</Col>
+            </Row>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+    );
+  };
 
   render() {
     const { classrooms } = this.state;
@@ -33,12 +114,15 @@ class TeacherClassrooms extends Component {
             </Row>
             <Row className="rowAccount">
               {Object.keys(classrooms).length > 0 ? (
-                <ClassroomList classrooms={classrooms} />
+                this.classroomList(classrooms)
               ) : (
                 <NoClassrooms />
               )}
             </Row>
           </Col>
+          {this.state.showClassroom
+            ? this.classroom(this.state.selectedClassroom)
+            : null}
           <Col className="insideBox">
             <Row className="rowAccount">
               <CreateClassroom />
@@ -55,23 +139,6 @@ const condition = authUser => !!authUser;
 
 const NoClassrooms = () => (
   <p style={{ textAlign: "left" }}>Du har ingen klasserom ennå</p>
-);
-
-const ClassroomList = ({ classrooms }) => (
-  <ListGroup variant="flush" style={{ width: "80%" }}>
-    {Object.keys(classrooms).map((name, i) => (
-      <ListGroup.Item
-        key={i}
-        style={{ textAlign: "left" }}
-        action
-        variant="warning"
-      >
-        <Row>
-          <Col>{name}</Col>
-        </Row>
-      </ListGroup.Item>
-    ))}
-  </ListGroup>
 );
 
 export default withFirebase(withAuthorization(condition)(TeacherClassrooms));
